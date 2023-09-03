8c7c805bfd3c2210ab3085eea68b500834e30bc8
Mit Shah <myshah2@myseneca.ca>
2023-09-03 00:43:46 -0400

    commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import travelSite from '../assets/portfolio/travel.jpg'
 import movix from '../assets/portfolio/movix.jpg'
-import navbar from '../assets/portfolio/navbar.jpg'
-import reactParallax from '../assets/portfolio/reactParallax.jpg'
-import reactSmooth from '../assets/portfolio/reactSmooth.jpg'
+// import navbar from '../assets/portfolio/navbar.jpg'
+// import reactParallax from '../assets/portfolio/reactParallax.jpg'
+// import reactSmooth from '../assets/portfolio/reactSmooth.jpg'
 import reactWeather from '../assets/portfolio/reactWeather.jpg'
 
 const Portfolio = () => {
@@ -17,24 +17,24 @@ const Portfolio = () => {
         },
         {
             id: 2,
-            src: reactParallax
-        },
-        {
-            id: 3,
-            src: navbar
-        },
-        {
-            id: 4,
-            src: reactSmooth
-        },
-        {
-            id: 5,
             src: movix,
             demo: "https://sm-movix-eecu9iaet-smeet5577.vercel.app/",
-            code: "https://github.com/smeet5577/sm-movix",
+            code: "https://github.com/smeet5577/sm-movix", 
         },
+        // {
+        //     id: 3,
+        //     src: navbar
+        // },
+        // {
+        //     id: 4,
+        //     src: reactSmooth
+        // },
+        // {
+        //     id: 5,
+        //     src: reactParallax
+        // },
         {
-            id: 6,
+            id: 3,
             src: reactWeather 
         },
     ]
